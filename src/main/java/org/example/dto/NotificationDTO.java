59fb288e842d9a79749ebbfff400a36ef148fc5f
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2024-04-05 17:40:42 -0300

    feat: create AppConfig, NotificationDTO, NotificationService

---
+++ src/main/java/org/example/dto/NotificationDTO.java
@@ -0,0 +1,4 @@
+package org.example.dto;
+
+public record NotificationDTO(String email, String message) {
+}
---
+++ src/main/java/org/example/services/NotificationService.java
@@ -0,0 +1,28 @@
+package org.example.services;
+
+import org.example.Domain.user.User;
+import org.example.dto.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void senderNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("http://o4d9z.mocklab.io/notify", notificationRequest, String.class);
+
+        if(notificationResponse.getStatusCode() == HttpStatus.OK){
+            System.out.println("Erro Notificação");
+            throw new Exception("Serviço denotificação fora do ar");
+
+        }
+   
+    }
+}
